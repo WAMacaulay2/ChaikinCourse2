e47b4c5c941ead8e764342504032112ea50b9653
WAMacaulay2 <alex@spotted-cow.net>
2020-10-02 13:39:15 -0500

    Did first two bonus requirements for assignment 1.

--- mod1-solution/js/app.js
+++ mod1-solution/js/app.js
@@ -9,11 +9,18 @@
     $scope.message = "";
     $scope.lunchList = "";
 
+    $scope.msgColor = "black";
+    $scope.borderColor = "black";
+
     $scope.checkList = function () {
       if ($scope.lunchList == '') {
         $scope.message = "Please enter data first";
+        $scope.borderColor = "red";
+        $scope.msgColor = "black";
         return;
       }
+
+      $scope.borderColor = "green";
 
       var list = $scope.lunchList.split(',');
       var numItems = list.length;
@@ -21,9 +28,11 @@
 
       if (numItems > 3) {
         $scope.message = "Too much!";
+        $scope.msgColor = "red";
       }
       else {
         $scope.message = "Enjoy!";
+        $scope.msgColor = "green";
       }
     };
 
