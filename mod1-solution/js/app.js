dd82a5dfdfe75c166ddff8f01226a19b0419ab0d
WAMacaulay2 <alex@spotted-cow.net>
2020-10-02 13:59:14 -0500

    Implemented final bonus requirement for assignment 1.

--- mod1-solution/js/app.js
+++ mod1-solution/js/app.js
@@ -10,31 +10,47 @@
     $scope.lunchList = "";
 
     $scope.msgColor = "black";
-    $scope.borderColor = "black";
+    $scope.inputColor = "black";
 
-    $scope.checkList = function () {
+    $scope.CheckList = function () {
+      //First, check that something was actually entered:
       if ($scope.lunchList == '') {
         $scope.message = "Please enter data first";
-        $scope.borderColor = "red";
-        $scope.msgColor = "black";
+        SetColors("red", "black");
         return;
       }
 
-      $scope.borderColor = "green";
-
-      var list = $scope.lunchList.split(',');
-      var numItems = list.length;
-      console.log("Number of items: " + numItems);
+      var list = $scope.lunchList.split(',');//Commas are our separator character.
+      console.log("Item list size: " + numItems);
+      var numItems = CountItems(list);//This function only counts non-empty items.
 
       if (numItems > 3) {
         $scope.message = "Too much!";
-        $scope.msgColor = "red";
+        SetColors("green", "red");
       }
       else {
         $scope.message = "Enjoy!";
-        $scope.msgColor = "green";
+        SetColors("green", "green");
       }
     };
+
+    function SetColors(input, msg) {
+      $scope.msgColor = msg;
+      $scope.inputColor = input;
+    }
+
+    function CountItems(list) {
+      var numItems = 0;
+      for (var i = 0; i < list.length; i++) {
+        console.log("Item " + i + ": " + list[i].trim() + ";");
+        if (list[i].trim() != '') { //string.trim() removes whitespace on edges of string; an all-whitespace string will become blank.
+          numItems++;//Only increase count when string has more than whitespace in it.
+        }
+      }
+
+      console.log("Actual items: " + numItems);
+      return numItems;
+    }
 
   }
 })();
