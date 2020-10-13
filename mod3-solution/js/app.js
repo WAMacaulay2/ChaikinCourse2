87754fa7081ee99fc9cf4dc649ac30a865fad2e9
WAMacaulay2 <alex@spotted-cow.net>
2020-10-13 12:59:41 -0500

    Completed Assignment 3 (I think)

--- mod3-solution/js/app.js
+++ mod3-solution/js/app.js
@@ -11,16 +11,23 @@
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
     // MenuSearchService.getMatchedMenuItems('chicken'); // Test of search function.
-    this.found = [];// Initialize found list.
+    var control = this;
+    control.found = [];// Initialize found list.
 
-    this.searchTerm = "";
+    control.searchTerm = "";
 
-    this.searchMenu = function() {
-      this.found = MenuSearchService.getMatchedMenuItems(this.searchTerm);// Use service to get array of matching items.
+    control.searchMenu = function() {
+      var promise = MenuSearchService.getMatchedMenuItems(this.searchTerm);// Use service to get array of matching items.
+
+      promise.then(function(response) {
+        // console.log("Back in controller");
+        // console.log(response);
+        control.found = response;
+      })
     }
 
     this.removeItem = function(index) {
-      found.splice(index, 1);// Splice out the item that was to be removed.
+      this.found.splice(index, 1);// Splice out the item that was to be removed.
     }
   }
 
@@ -34,17 +41,21 @@
 
       return $http({ method: 'GET', url: davidChuURL }).then(function(result) {// Make http call and set callback handler.
         var foundItems = result.data.menu_items;// Extract full menu from returned data.
-        console.log(foundItems);
+        // console.log(foundItems);
 
         for(var i = 0; i < foundItems.length; i++) {
-          if(foundItems[i].description.indexOf(searchTerm) == -1) {
+          if(foundItems[i].description.indexOf(searchTerm) === -1) {
+            // console.log("Discard: " + foundItems[i].short_name + " "
+            // + foundItems[i].name + ": " + foundItems[i].description);// Test to see that search is working.
             foundItems.splice(i, 1);// Splice out item if search term wasn't found in it.
+            i--;// We need to back up if we delete one.
           }
           // else {
-          //   console.log(foundItems[i].description);// Test to see that search is working.
+          //   console.log("Keep: " + foundItems[i].short_name + " "
+          //   + foundItems[i].name + ": " + foundItems[i].description);// Test to see that search is working.
           // }
         }
-        console.log(foundItems);
+        // console.log(foundItems);
         return foundItems;// Return an array of only items that match.
       }).catch(function(error) {
         console.log("Connection error");
